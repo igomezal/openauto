469f15af3f418d358cfa9f7c93670e229ae1f1ef
Ivan Gomez Alonso <yvangomezalonso@gmail.com>
2018-12-28 17:05:37 +0100

    fix: add return type

--- include/f1x/openauto/autoapp/UI/MainWindow.hpp
+++ include/f1x/openauto/autoapp/UI/MainWindow.hpp
@@ -41,7 +41,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
-    shutDownButtonPressed()
+    void shutDownButtonPressed()
 
 signals:
     void exit();

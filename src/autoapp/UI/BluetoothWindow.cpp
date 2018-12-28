e27a8d5643c8142154abc54f3ab82295af674bae
Ivan Gomez Alonso <yvangomezalonso@gmail.com>
2018-12-29 00:31:43 +0100

    fix; change to StretchLastSection

--- src/autoapp/UI/BluetoothWindow.cpp
+++ src/autoapp/UI/BluetoothWindow.cpp
@@ -14,7 +14,7 @@ BluetoothWindow::BluetoothWindow(QWidget *parent)
     : QWidget(parent), ui_(new Ui::BluetoothWindow)
 {
     ui_->setupUi(this);
-    ui_->tableWidget->horizontalHeader()->setResizeMode(QHeaderView::Stretch);
+    ui_->tableWidget->horizontalHeader()->setStretchLastSection(true);
     connect(ui_->pushButtonCancel, &QPushButton::clicked, this, &BluetoothWindow::close);
 }
 
